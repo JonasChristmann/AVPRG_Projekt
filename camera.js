abef3d418adffb3d18d37104eb887ab1739242c4
Jonas <ChristmannJonas@web.de>
2022-11-28 17:06:57 +0100

    Symbole und Caption fertig

--- camera.js
+++ camera.js
@@ -1,5 +1,12 @@
-const webCamElement = document.getElementById("webCam");
-const canvasElement = document.getElementById("canvas");
-const webcam = new Webcam(webCamElement, "user", canvasElement );
-print("yes");
-webcam.start();
+const canvas2 = document.querySelector("#canvas2");
+const ctx2 = canvas2.getContext('2d');
+const video = document.querySelector("video");
+
+video.addEventListener("play", () => {
+    function step() {
+        ctx2.drawImage(video, 0, 0, canvas2.width, canvas2.height);
+        requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
+});
+
--- segments.js
+++ segments.js
@@ -3,7 +3,6 @@ function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-
     return [x,y]
 }
 
@@ -19,6 +18,29 @@ let coordinates = [];
 let firstRect = true;
 let first = true;
 
+
+const hihatImage = new Image(50, 50);
+hihatImage.src = "./images/hi-hat.png";
+
+const cymbalImage = new Image(50, 50);
+cymbalImage.src = "./images/cymbal.png";
+
+const floorTomImage = new Image(50, 50);
+floorTomImage.src = "./images/floor-tom.png";
+
+const kickImage = new Image(50, 50);
+kickImage.src = "./images/kick.png";
+
+const snareImage = new Image(50, 50);
+snareImage.src = "./images/snare-drum.png";
+
+const tomLeftImage = new Image(50, 50);
+tomLeftImage.src = "./images/tom-left.png";
+
+const tomRightImage = new Image(50, 50);
+tomRightImage.src = "./images/tom-right.png";
+
+
 segmentCaption(coordinates.length);
 
 canvas.addEventListener('mousedown', (e)=>{
@@ -31,6 +53,7 @@ canvas.addEventListener('mousedown', (e)=>{
 
         
         ctx.fillRect(getCursorPosition(canvas, e)[0]-5, getCursorPosition(canvas, e)[1]-5, 10, 10);
+
 
         if (rectClicks >= 1 && firstRect) {
             ctx.lineTo(...getCursorPosition(canvas, e));
@@ -48,6 +71,8 @@ canvas.addEventListener('mousedown', (e)=>{
 
         rectClicks++;
 
+        singleCoordinates.push(getCursorPosition(canvas, e));
+
         if (rectClicks == 4 ) {
             rectClicks = 0;
             coordinates.push(singleCoordinates);
@@ -56,13 +81,11 @@ canvas.addEventListener('mousedown', (e)=>{
             ctx.stroke();
             firstRect = false;
             segmentCaption(coordinates.length);
+            drawInCenter(coordinates.length-1);
 
         }
 
-        singleCoordinates.push(getCursorPosition(canvas, e));
-
         console.log(coordinates);
-
     }
 
 });
@@ -98,9 +121,79 @@ function segmentCaption(i){
         case 6:
             text.innerHTML = "Define the Cymbal section";
             break;
+        case 7: 
+            text.innerHTML = "All sections Set, ready to play "
+            break;
 
         default:
             break;
     }
 
+}
+
+function drawInCenter(i){
+
+    let sumX = 0;
+    let sumY = 0;
+
+    
+    coordinates[[coordinates.length-1]].forEach(element => {
+        sumX += element[0];
+        sumY += element[1];
+    });
+
+    let width = 50;
+    let height = 50;
+
+    let x = sumX / 4 - width / 2;
+    let y = sumY / 4 - height / 2;
+
+
+        
+    switch (i) {
+        case 0:
+            // text.innerHTML = "Define the Kick section";
+            ctx.drawImage(kickImage, x, y, width, height);
+
+            break;
+
+        case 1:
+            // text.innerHTML = "Define the Left-Tom section";
+            ctx.drawImage(tomLeftImage, x, y, width, height);
+
+            break;
+
+        case 2:
+            // text.innerHTML = "Define the Right-Tom section";
+            ctx.drawImage(tomRightImage, x, y, width, height);
+
+            break;
+        
+        case 3:
+            // text.innerHTML = "Define the Snare section";
+            ctx.drawImage(snareImage, x, y, width, height);
+
+            break;
+
+        case 4:
+            // text.innerHTML = "Define the Hi-Hat section";
+            ctx.drawImage(hihatImage, x, y, width, height);
+
+            break;
+
+        case 5:
+            // text.innerHTML = "Define the Floor Tom section";
+            ctx.drawImage(floorTomImage, x, y, width, height);
+
+            break;
+             
+        case 6:
+            // text.innerHTML = "Define the Cymbal section";
+            ctx.drawImage(cymbalImage, x, y, width, height);
+
+            break;
+
+        default:
+            break;
+    }
 }
